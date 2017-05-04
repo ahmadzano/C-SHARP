716217b9f57ae138a9001296013dbed8014e02d8
ahmaddo <a.ahmad@outlook.de>
2017-05-04 19:34:30 +0200

    MwS Bechechner

---
+++ MwSBerechner/ConsoleApplication3/MWSCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// Zusammenfassungsbeschreibung für Class1
+/// </summary>
+/// 
+
+public class MWSCalculator
+{
+    double bruttoPreis;
+    double MWS;
+
+    public MWSCalculator(double bruttoPreis)
+    {
+        this.bruttoPreis = bruttoPreis;
+        this.calculateMWS();
+    }
+
+        private void calculateMWS()
+    {
+        this.MWS = (this.bruttoPreis * 19) / 100; 
+    }
+
+    public double getMWS()
+    {
+        return this.MWS;
+    }
+}
---
+++ MwSBerechner/ConsoleApplication3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWSBecherner
+
+{
+    class MWSBecherner
+    {
+        static void Main(string[] args)
+        {
+            double bruttoPreis = 200.00;
+            MWSCalculator mws = new MWSCalculator(bruttoPreis);
+            Console.WriteLine(mws.getMWS());
+            Console.ReadLine();
+        }
+    }
+}
