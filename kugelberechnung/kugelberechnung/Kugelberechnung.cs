d9b411fe810e5da8f80c4c6ca0ddf2e9f82dc4a5
ahmaddo <a.ahmad@outlook.de>
2017-05-04 19:30:09 +0200

    Kugelberechnung Aufgabe

---
+++ kugelberechnung/kugelberechnung/Kugelberechnung.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kugelberechnung
+{
+    class Kugelberechnung
+    {
+        double radius;
+        double umfang;
+        double oeberflaeche;
+
+        public Kugelberechnung(double radius)
+        {
+            this.radius = radius;
+            this.calculateUmfang();
+            this.calculateOeberflaeche();
+        }
+
+        private void calculateUmfang()
+        {
+            this.umfang = this.radius * 2;
+        }
+
+        private void calculateOeberflaeche()
+        {
+            this.oeberflaeche = this.radius * this.radius;
+        }
+
+        public double getUmfang()
+        {
+            return this.umfang;
+        }
+
+        public double getOeberflaeche()
+        {
+            return this.oeberflaeche;
+        }
+
+    }
+}
---
+++ kugelberechnung/kugelberechnung/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kugelberechnung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 6;
+            Kugelberechnung kugel = new Kugelberechnung(radius);
+            Console.WriteLine(kugel.getUmfang());
+            Console.WriteLine(kugel.getOeberflaeche());
+            Console.ReadLine();
+
+        }
+    }
+}
