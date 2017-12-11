a2df3b79afa9a039c74e9755a66689e399857b37
ahmaddo <a.ahmad@outlook.de>
2017-12-11 20:30:34 +0100

    create Artikel class

---
+++ Bestellsystem/Bestellsystem/Bestellsystem/Artikel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bestellsystem
+{
+    abstract class Artikel
+    {
+        int Nummer { set; get; }
+        String Bezeichnung { set; get; }
+        String Verpackungsatr { set; get; }
+        Double Lagerstand { set; get; }
+        Double LagerMidestBestand { set; get; }
+        Lager Lager { set; get; }
+        Lieferant Liferant { set; get; }
+        Double Einkaufspreis { set; get; }
+        int Lieferziet { set; get; }
+
+
+    }
+}
