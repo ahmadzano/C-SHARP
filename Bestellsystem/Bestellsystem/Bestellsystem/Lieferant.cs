7bba7fe565ada64ade01db033771c2f6ef58cfa6
ahmaddo <a.ahmad@outlook.de>
2017-12-11 20:41:28 +0100

    create Lieferant class

---
+++ Bestellsystem/Bestellsystem/Bestellsystem/Lieferant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bestellsystem
+{
+    class Lieferant
+    {
+        int Id { set; get; }
+        String Name { set; get; }
+        int Telefonnummer { set; get; }
+        Adresse Adresse { set; get; }
+    }
+}
