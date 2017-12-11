a450678afdbb3aee3ee67451186684f5d622a757
ahmaddo <a.ahmad@outlook.de>
2017-12-11 20:34:34 +0100

    create foor artikel class

---
+++ Bestellsystem/Bestellsystem/Bestellsystem/FoodArtikel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bestellsystem
+{
+    class FoodArtikel: Artikel
+    {
+        DateTime haltbarkeitsDauer { set; get; }
+        String Kuehlungsart { set; get; }
+    }
+}
