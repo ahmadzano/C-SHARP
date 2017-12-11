dfa8d21af32713ea1c6f1e1cb2becf0d32578e09
ahmaddo <a.ahmad@outlook.de>
2017-12-11 20:36:57 +0100

    create bestellposition class

---
+++ Bestellsystem/Bestellsystem/Bestellsystem/Bestellposition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bestellsystem
+{
+    class Bestellposition
+    {
+        Artikel Artikel { set; get; }
+        Double Anzahl { set; get; }
+    }
+}
