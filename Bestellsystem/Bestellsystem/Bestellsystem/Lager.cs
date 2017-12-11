eb31e60acf448ab1f5a70d4e5f03e04ebe1d1ae7
ahmaddo <a.ahmad@outlook.de>
2017-12-11 20:25:11 +0100

    create Lager class

---
+++ Bestellsystem/Bestellsystem/Bestellsystem/Lager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bestellsystem
+{
+    class Lager
+    {
+        private int Number { set; get; }
+        private String Name { set; get; }
+
+        public Lager(int number, string name)
+        {
+            this.Number = number;
+            this.Name = name;
+        }
+    }
+}
