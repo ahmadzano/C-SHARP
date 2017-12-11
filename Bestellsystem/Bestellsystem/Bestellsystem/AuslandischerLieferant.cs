34480f2c84116a89d96950b5695cc234ea9f83e8
ahmaddo <a.ahmad@outlook.de>
2017-12-11 20:43:22 +0100

    create auslandischer  lieferantclass

---
+++ Bestellsystem/Bestellsystem/Bestellsystem/AuslandischerLieferant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bestellsystem
+{
+    class AuslandischerLieferant : Lieferant
+    {
+        String Land { set; get; }
+        bool EUMitglied { set; get; }
+    }
+}
