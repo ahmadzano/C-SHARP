3a777c92db7aa7b6ed4d4ca60a20fae47ccad945
ahmaddo <a.ahmad@outlook.de>
2017-12-11 20:40:26 +0100

    create adresse class

---
+++ Bestellsystem/Bestellsystem/Bestellsystem/Adresse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bestellsystem
+{
+    class Adresse
+    {
+        String Strasse { set; get; }
+        int Hausnummer { set; get; }
+        int PLZ { set; get; }
+        String Ort { set; get; }
+    }
+}
