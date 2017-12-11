e3e831d0c2eea29e025cf72c3c5ba160f56ee8d4
ahmaddo <a.ahmad@outlook.de>
2017-12-11 20:32:32 +0100

    create non-food artikel class

---
+++ Bestellsystem/Bestellsystem/Bestellsystem/NonFoodArtikel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bestellsystem
+{
+    class NonFoodArtikel : Artikel
+    {
+        bool isSperrgut { set; get; }
+    }
+}
